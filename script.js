accdede749c3a4e7b82bb360c3852b9fce8cef97
kathyychoi <66241066+kathyychoi@users.noreply.github.com>
2021-09-19 07:12:13 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
   $("#submit-button").click(function() {
-      let hours = [$("#sleep-1").val(), $("#sleep-2").val(), $("#sleep-3").val(), $("#sleep-4").val(), $("#sleep-5").val(), $("#sleep-6").val(), $("#sleep-7").val()];
+      var hours = [$("#sleep-1").val(), $("#sleep-2").val(), $("#sleep-3").val(), $("#sleep-4").val(), $("#sleep-5").val(), $("#sleep-6").val(), $("#sleep-7").val()];
       var average = 0;
       for (let i = 0; i < 7; i++) {
         average += hours[i];
