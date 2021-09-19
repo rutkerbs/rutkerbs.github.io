364815e17cdaf3e35c70cc3ad0bec1177ea2027f
kathyychoi <66241066+kathyychoi@users.noreply.github.com>
2021-09-18 19:09:01 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 
   $("#submit-button").click(function() {
-      const url = "http://localhost:5000/submit";
       let hours = [$("#sleep-1").val(), $("#sleep-2").val(), $("#sleep-3").val(), $("#sleep-4").val(), $("#sleep-5").val(), $("#sleep-6").val(), $("#sleep-7").val()];
       var average = 0
       for (let i = 0; i < 7; i++) {
