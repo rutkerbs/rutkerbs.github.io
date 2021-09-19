8a3821a8a88c3f6205382236b64fc2d7c1c3b1e1
kathyychoi <66241066+kathyychoi@users.noreply.github.com>
2021-09-19 07:17:11 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
       var hours = [$("#sleep-1").val(), $("#sleep-2").val(), $("#sleep-3").val(), $("#sleep-4").val(), $("#sleep-5").val(), $("#sleep-6").val(), $("#sleep-7").val()];
       var average = 0;
       for (let i = 0; i < 7; i++) {
-        average += hours[i];
+        average += parseInt(hours[i]);
       }
       average /= 7;
       alert("Average hours of sleep: " + average);
