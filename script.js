43731e346f753ffe19c94c392639a95da72377c9
kathyychoi <66241066+kathyychoi@users.noreply.github.com>
2021-09-18 19:05:44 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
 
   $("#submit-button").click(function() {
-      let hours = []
+      const url = "http://localhost:5000/submit";
+      let hours = [$("#sleep-1").val(), $("#sleep-2").val(), $("#sleep-3").val(), $("#sleep-4").val(), $("#sleep-5").val(), $("#sleep-6").val(), $("#sleep-7").val()];
+      var average = 0
       for (let i = 0; i < 7; i++) {
-        hours.push($("#sleep-box").val());
+        average += hours[i];
       }
-      alert("Average hours of sleep: " + hours[0])
+      average /= 7;
+      alert("Average hours of sleep: " + average)
   });
   
 });
