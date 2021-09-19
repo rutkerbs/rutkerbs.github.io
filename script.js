574ae743e7b6484a5fc031c0f1dd43387e3f5c32
kathyychoi <66241066+kathyychoi@users.noreply.github.com>
2021-09-19 07:35:07 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ $(document).ready(function() {
   $("#submit-button").click(function() {
       var hours = [$("#sleep-1").val(), $("#sleep-2").val(), $("#sleep-3").val(), $("#sleep-4").val(), $("#sleep-5").val(), $("#sleep-6").val(), $("#sleep-7").val()];
       var average = 0;
-      var standard_deviation = 0;
+      var deviation = 0;
       for (let i = 0; i < 7; i++) {
         average += parseInt(hours[i]);
       }
       average /= 7;
-      standard_deviation = 4
-      alert("Average hours of sleep: " + average + "Standard deviation: " + standard_deviation);
+      deviation = 4
+      alert("Average hours of sleep: " + average + "Standard deviation: " + deviation);
   });
   
 });
